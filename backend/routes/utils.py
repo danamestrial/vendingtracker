7edd3c38af8f6be1a265276dc7a5faf07c2cee8d
danamestrial <chayapolbun@gmail.com>
2023-01-16 22:10:01 +0700

    feat: created query methods for easier querying in other files

--- backend/routes/utils.py
+++ backend/routes/utils.py
@@ -1,7 +1,22 @@
-from flask import current_app as app, session, redirect
+from flask import current_app as app, jsonify
 from flask_mysqldb import MySQL
 
 mysql = MySQL(app)
 
 def status(s: bool):
     return dict({'status': s})
+
+def select_query(query: str):
+    with mysql.connection.cursor() as cur:
+        cur.execute(query)
+        results = cur.fetchall()
+        return jsonify(results)
+
+def value_query(query: str, values: tuple):
+    try:
+        with mysql.connection.cursor() as cur:
+            cur.execute(query, values)
+            cur.connection.commit()
+        return status(True)
+    except:
+        return status(False)
