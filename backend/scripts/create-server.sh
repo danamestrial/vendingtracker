2cd70a9e41644b4233c97190cbc283d0427c4b58
danamestrial <chayapolbun@gmail.com>
2023-01-16 23:08:49 +0700

    fix: add password args to create-server.sh

--- backend/scripts/create-server.sh
+++ backend/scripts/create-server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run --name mysql-db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mypassword -d mysql
+# If no args, return usage
+if [ $# -lt 1 ]; then
+	echo "Usage: ./create-server.sh {password}"
+	exit
+
+docker run --name mysql-db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$1 -d mysql
