4a2b5349182cbbb6a7097b03bafe676152088086
danamestrial <chayapolbun@gmail.com>
2023-01-16 22:09:16 +0700

    feat: added crud api for items

--- backend/routes/items_routes.py
+++ backend/routes/items_routes.py
@@ -12,6 +12,29 @@ def list_all():
         print(results)
         return jsonify(results)
 
-# @item.route("/add_item", methods=['POST'])
-# def add_item():
-#     with mysql.connection.cursor() as cur:
+@item.route("/add-item", methods=['POST'])
+def add_item():
+    with mysql.connection.cursor() as cur:
+        args = request.args
+        mysqlquery = '''insert into items(name) values(%s)'''
+        cur.execute(mysqlquery, (args.get('name'),))
+        cur.connection.commit()
+    return status(True)
+
+@item.route("/remove-item", methods=['POST'])
+def remove_item():
+    with mysql.connection.cursor() as cur:
+        args = request.args
+        mysqlquery = '''delete from items where id = %s'''
+        cur.execute(mysqlquery, args.get('id'))
+        cur.connection.commit()
+    return status(True)
+
+@item.route("/update-item", methods=['POST'])
+def update_item():
+    with mysql.connection.cursor() as cur:
+        args = request.args
+        mysqlquery = '''update items set name = %s where id = %s'''
+        cur.execute(mysqlquery, (args.get('name'), args.get('id')))
+        cur.connection.commit()
+    return status(True)
