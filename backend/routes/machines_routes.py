18e7a0fb48e87df6dd1efc7b2f1a62bd71e6b81c
danamestrial <chayapolbun@gmail.com>
2023-01-29 23:21:30 +0700

    fix: change naming convention of var "mysqlquery" -> "sqlquery"

--- backend/routes/machines_routes.py
+++ backend/routes/machines_routes.py
@@ -6,35 +6,35 @@ machine = Blueprint('machine', __name__, url_prefix='/machine')
 
 @machine.route('/list-all', methods=['GET'])
 def list_all():
-    mysqlquery = '''select * from machines'''
-    return select_query(mysqlquery)
+    sql_query = '''select * from machines'''
+    return select_query(sql_query)
 
 
 @machine.route('/add', methods=['POST'])
 def add_machine():
     args = request.args
-    mysqlquery = '''insert into machines(handle, location, status) values (%s, %s, %s)'''
-    return value_query(mysqlquery, (args.get("handle"), args.get("location"), args.get("status", 0)))
+    sql_query = '''insert into machines(handle, location, status) values (%s, %s, %s)'''
+    return value_query(sql_query, (args.get("handle"), args.get("location"), args.get("status", 0)))
 
 
 @machine.route("/delete", methods=['DELETE'])
 def delete_machine():
     args = request.args
-    mysqlquery = '''delete from machines where id = %s'''
-    return value_query(mysqlquery, (args.get('machine_id'),))
+    sql_query = '''delete from machines where id = %s'''
+    return value_query(sql_query, (args.get('machine_id'),))
 
 
 @machine.route("/update", methods=['PATCH'])
 def update_machine():
     args = request.args
-    mysqlquery = '''
+    sql_query = '''
     update machines set
     handle = ifnull(%s, handle),
     location = ifnull(%s, location),
     status = ifnull(%s, status)
     where id = %s
     '''
-    return value_query(mysqlquery, \
+    return value_query(sql_query, \
                        (args.get("handle"), \
                         args.get("location"), \
                         args.get("status"), \
--- backend/routes/stock_routes.py
+++ backend/routes/stock_routes.py
@@ -6,32 +6,32 @@ stock = Blueprint('stock', __name__, url_prefix='/stock')
 
 @stock.route("/list", methods=['GET'])
 def list_all_stocks():
-    mysqlquery = '''
+    sql_query = '''
     select item_id, name as "item_name", quantity
     from stocks, items
     where item_id = items.id AND machine_id = %s'''
-    return select_query(mysqlquery, (request.args.get('machine_id'),))
+    return select_query(sql_query, (request.args.get('machine_id'),))
 
 
 @stock.route("/add", methods=['POST'])
 def add_stock():
     args = request.args
-    mysqlquery = '''insert into stocks(machine_id, item_id, quantity) values(%s, %s, %s)'''
-    return value_query(mysqlquery, (args.get('machine_id'), args.get('item_id'), args.get('quantity')))
+    sql_query = '''insert into stocks(machine_id, item_id, quantity) values(%s, %s, %s)'''
+    return value_query(sql_query, (args.get('machine_id'), args.get('item_id'), args.get('quantity')))
 
 
 @stock.route("/delete", methods=['DELETE'])
 def remove_stock():
     args = request.args
-    mysqlquery = '''delete from stocks where machine_id = %s and item_id = %s'''
-    return value_query(mysqlquery, (args.get('machine_id'), args.get('item_id')))
+    sql_query = '''delete from stocks where machine_id = %s and item_id = %s'''
+    return value_query(sql_query, (args.get('machine_id'), args.get('item_id')))
 
 
 @stock.route("/update", methods=['PATCH'])
 def update_stock():
     args = request.args
-    mysqlquery = '''
+    sql_query = '''
     update stocks set
     quantity = ifnull(%s, quantity)
     where machine_id = %s and item_id = %s'''
-    return value_query(mysqlquery, (args.get('quantity'), args.get('machine_id'), args.get('item_id')))
+    return value_query(sql_query, (args.get('quantity'), args.get('machine_id'), args.get('item_id')))
