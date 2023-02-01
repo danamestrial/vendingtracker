e54cea4437eeeca60b025b5f0f61c906e796e922
danamestrial <chayapolbun@gmail.com>
2023-02-01 12:27:27 +0700

    feat: Update Tests to utilise test_client, Added test for utils file

--- backend/tests/test_items_routes.py
+++ backend/tests/test_items_routes.py
@@ -1,55 +1,39 @@
-import requests
-import os
-from dotenv import load_dotenv
-from secrets import token_urlsafe
-
-load_dotenv()
-
-host = os.getenv('HOST', '127.0.0.1')
-port = os.getenv('PORT', '5000')
-base_url = f"http://{host}:{port}"
-rand_handle = token_urlsafe(16)
-rand_handle_new = token_urlsafe(16)
-
-
-def test_list_all():
-    response = requests.get(f"{base_url}/item/list-all")
+def test_list_all(client):
+    response = client.get("/item/list-all")
     assert response.status_code == 200
-    response_json = response.json()
+    response_json = response.json
     assert response_json['status']
     assert type(response_json['message']) == list
 
 
-def test_add_item():
-    args = {"item_name": rand_handle}
-    response = requests.post(f"{base_url}/item/add", params=args)
+def test_add_item(client):
+    args = {"item_name": "TEST_ITEM"}
+    response = client.post("/item/add", query_string=args)
     assert response.status_code == 200
-    response_json = response.json()
+    response_json = response.json
     assert response_json['status']
     assert response_json['message'] == ""
 
 
-def test_update_item():
-    list_response = requests.get(f"{base_url}/item/list-all")
-    item_list = list_response.json()['message']
-    unique_id = list(filter(lambda x: x["name"] == rand_handle, item_list))[0]['id']
-    args = {"item_name": rand_handle_new, "item_id": unique_id}
-    response = requests.patch(f"{base_url}/item/update", params=args)
+def test_update_item(client):
+    list_response = client.get("/item/list-all")
+    item_list = list_response.json['message']
+    unique_id = list(filter(lambda x: x["name"] == "TEST_ITEM", item_list))[0]['id']
+    args = {"item_name": "NEW_TEST_ITEM", "item_id": unique_id}
+    response = client.patch("/item/update", query_string=args)
     assert response.status_code == 200
-    response_json = response.json()
-    print(response_json)
+    response_json = response.json
     assert response_json['status']
     assert response_json['message'] == ""
 
 
-def test_delete_item():
-    list_response = requests.get(f"{base_url}/item/list-all")
-    item_list = list_response.json()['message']
-    unique_id = list(filter(lambda x: x["name"] == rand_handle_new, item_list))[0]['id']
+def test_delete_item(client):
+    list_response = client.get("/item/list-all")
+    item_list = list_response.json['message']
+    unique_id = list(filter(lambda x: x["name"] == "NEW_TEST_ITEM", item_list))[0]['id']
     args = {"item_id": unique_id}
-    response = requests.delete(f"{base_url}/item/delete", params=args)
+    response = client.delete("/item/delete", query_string=args)
     assert response.status_code == 200
-    response_json = response.json()
-    print(response_json)
+    response_json = response.json
     assert response_json['status']
     assert response_json['message'] == ""
--- backend/tests/test_machines_routes.py
+++ backend/tests/test_machines_routes.py
@@ -1,55 +1,38 @@
-import requests
-import os
-from dotenv import load_dotenv
-from secrets import token_urlsafe
-
-load_dotenv()
-
-host = os.getenv('HOST', '127.0.0.1')
-port = os.getenv('PORT', '5000')
-base_url = f"http://{host}:{port}"
-rand_dict = {"handle": token_urlsafe(16), "location": token_urlsafe(16)}
-rand_dict_new = {"handle": token_urlsafe(16), "location": token_urlsafe(16)}
-
-
-def test_list_all():
-    response = requests.get(f"{base_url}/machine/list-all")
+def test_list_all(client):
+    response = client.get("/machine/list-all")
     assert response.status_code == 200
-    response_json = response.json()
-    assert response_json['status']
+    response_json = response.json
     assert type(response_json['message']) == list
 
 
-def test_add_machine():
-    args = {"handle": rand_dict["handle"], "location": rand_dict["location"], "status": 1}
-    response = requests.post(f"{base_url}/machine/add", params=args)
+def test_add_machine(client):
+    args = {"handle": "TEST", "location": "TEST", "status": 1}
+    response = client.post("/machine/add", query_string=args)
     assert response.status_code == 200
-    response_json = response.json()
+    json = response.json
+    assert json['status']
+    assert json['message'] == ""
+
+
+def test_update_machine(client):
+    list_response = client.get("/machine/list-all")
+    machine_list = list_response.json['message']
+    unique_id = list(filter(lambda x: x["handle"] == "TEST", machine_list))[0]['id']
+    args = {"handle": "NEWTEST", "location": "NEWTEST", "machine_id": unique_id}
+    response = client.patch("/machine/update", query_string=args)
+    assert response.status_code == 200
+    response_json = response.json
     assert response_json['status']
     assert response_json['message'] == ""
 
 
-def test_update_machine():
-    list_response = requests.get(f"{base_url}/machine/list-all")
-    machine_list = list_response.json()['message']
-    unique_id = list(filter(lambda x: x["handle"] == rand_dict["handle"], machine_list))[0]['id']
-    args = {"handle": rand_dict_new["handle"], "location": rand_dict_new["location"], "machine_id": unique_id}
-    response = requests.patch(f"{base_url}/machine/update", params=args)
+def test_delete_machine(client):
+    list_response = client.get("/machine/list-all")
+    machine_list = list_response.json['message']
+    unique_id = list(filter(lambda x: x["handle"] == "NEWTEST", machine_list))[0]['id']
+    args = {"machine_id": unique_id}
+    response = client.delete("/machine/delete", query_string=args)
     assert response.status_code == 200
-    response_json = response.json()
-    print(response_json)
-    assert response_json['status']
-    assert response_json['message'] == ""
-
-
-def test_delete_machine():
-    list_response = requests.get(f"{base_url}/machine/list-all")
-    machine_list = list_response.json()['message']
-    unique_id = list(filter(lambda x: x["handle"] == rand_dict_new["handle"], machine_list))[0]['id']
-    args = {"machine_id": int(unique_id)}
-    response = requests.delete(f"{base_url}/machine/delete", params=args)
-    assert response.status_code == 200
-    response_json = response.json()
-    print(response_json)
+    response_json = response.json
     assert response_json['status']
     assert response_json['message'] == ""
