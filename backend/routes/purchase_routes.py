b7aa77217aa6f5d04b802af0e54404937ec6e7c3
BlackTea13 <robertblum2001@gmail.com>
2023-02-08 00:59:43 +0700

    linted some files

--- backend/routes/purchase_routes.py
+++ backend/routes/purchase_routes.py
@@ -1,10 +1,9 @@
-from typing import Dict, Union
-
 from flask import Blueprint, request
 
-from .utils import select_query, value_query
+from .utils import select_query
 
 purchase = Blueprint("purchase", __name__)
+
 
 @purchase.route("/item/records", methods=["GET"])
 def get_product_time_stamp():
@@ -13,10 +12,8 @@ def get_product_time_stamp():
     return select_query(query, ((product_id),))
 
 
-
 @purchase.route("/machine/records", methods=["GET"])
 def get_machine_time_stamp():
     machine_id = request.args.get("machine_id")
     query = """ SELECT * FROM purchase WHERE machine_id = %s"""
     return select_query(query, ((machine_id),))
-
