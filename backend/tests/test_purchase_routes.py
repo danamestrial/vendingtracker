938bfd0193f3f0f1e086cdaefa97dcf5067a4a3c
BlackTea13 <robertblum2001@gmail.com>
2023-02-08 01:19:01 +0700

    added test file

---
+++ backend/tests/test_purchase_routes.py
@@ -0,0 +1,9 @@
+from flask.testing import FlaskClient
+
+
+def test_vending_machine_records(client: FlaskClient) -> None:
+    pass
+
+
+def test_product_records(client: FlaskClient) -> None:
+    pass
